c19f6d420546f8c9f96d7d6f2e4ce9d973236c56
agrublev <agrublev@f14dab8a-fd35-0c86-2b86-9bfbb5228c99>
2011-01-02 00:20:58 +0000

    some changes on carousel, but it will need to be done from scratch (maybe use the jquery tools one). jSlider fixes jPlaceholder fixes and updates jTabs fixes and updates to documentation

--- trunk/jPlaceholder/jPlaceholder.js
+++ trunk/jPlaceholder/jPlaceholder.js
@@ -1,40 +1,52 @@
-// jBar Plugin for jQuery - Version 0.1
+// jPlaceholder Plugin for jQuery - Version 0.2
 // by Angel Grablev for Enavu Web Development network (enavu.com)
 // Dual license under MIT and GPL :) enjoy
 /*
 
-To use simply call .jBar() on the element you wish like so:
-$("#content").jBar(); 
+To use simply call .jPlaceholder() on the element you wish like so:
+$("#content").jPlaceholder(); 
 
 you can specify the following options:
 css_class = allows you to specify the class for the placeholder
 */
 (function($) {
-$.fn.placeholder = function(options) {
+$.fn.jPlaceholder = function(options) {
 	var defaults = {css_class: "placeholder"};
 	var options = $.extend(defaults, options);  
+	// handle form being submitted by clearing the populated fields
+	$("form").submit(function(){
+		$("input").each(function(){
+			if ($(this).attr("placeholder") != undefined) {
+				if ($(this).attr("placeholder") == $(this).attr("value")) {	$(this).val(""); }
+			}
+		});
+	});
+	// loop through all the elements youve selected with the plugin
 	this.each(function() {
-		if ($(this).attr('placeholder') != undefined) {
-			var phvalue = $(this).attr("placeholder");
-			var currvalue = $(this).attr("value");
+		
+		var phvalue = $(this).attr("placeholder");
+		var currvalue = $(this).attr("value");
+		
+		if (phvalue != undefined) {
+			
 			if (phvalue == currvalue) {
 				$(this).addClass(options.css_class);
+			} else {
+				$(this).removeClass(options.css_class);
 			}
 			if (currvalue == "") {
 				$(this).addClass(options.css_class);
 				$(this).val(phvalue);
 			}
 			$(this).focus(function(){
-				var ph = $(this).attr("placeholder");
-				if (ph == $(this).val()) {
+				if (phvalue == $(this).val()) {
 					$(this).val("").removeClass(options.css_class);
 				}
 			});
 			
 			$(this).blur(function(){
-				var ph = $(this).attr("placeholder");
 				if ($(this).val() == "") {
-					$(this).val(ph).addClass(options.css_class);
+					$(this).val(phvalue).addClass(options.css_class);
 				}
 			});
 		}
